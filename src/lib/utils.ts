178a5249472732c067728526403d122c977808f4
msgetsonmynervez <msgetsonmynervez@gmail.com>
2026-03-11 17:51:03 -0700

    Delete src directory

--- src/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-// This helps manage CSS classes for your big A11y buttons
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
